0d5ff54ea7ce202343a534021de2f764999b6d89
Lawrence Santander <santa077@csusm.edu>
2019-08-07 09:34:31 -0700

    js push

--- Bitcoin Clicker/bitcoin.js
+++ Bitcoin Clicker/bitcoin.js
@@ -1,14 +1,20 @@
-redDwarfbutton = document.querySelector(".ItemPicture1")
+const redDwarf_upgrade = document.querySelector(".Upgrade_Sect1")
 
 let total = 0
 let red
 
-function harvest(add)
-  {
+function harvest(add) {
     total += add
-  };
+};
 
-function upgrade(cost,amount,add,lvl)
-{
-
+function upgrade(cost, amount, add, name) {
+    if (total >= cost) {
+      total -= cost;
+      amount += 1;
+      add += 1;
+      cost += add;
+      document.querySelector(Currency).innerHTML = "$" + total + " StarDust"
+      redDwarf_upgrade.innerHTML = "Upgrade: $" + cost + " StarDust"
+      console.log(total, cost, amount, add)
+    }
 };
