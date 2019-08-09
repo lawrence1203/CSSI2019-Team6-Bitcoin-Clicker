fe7445447b4f97989939ab16929e08f874dd9b29
Nathan Paz <paz017@csusm.edu>
2019-08-08 21:41:01 -0700

    final work on html and css to do :loading bar to all ten rows&linkmusic

--- Bitcoin Clicker/expierement/css.js
+++ Bitcoin Clicker/expierement/css.js
@@ -1,14 +1,4 @@
-(function() {
-  for (var i=0; i<50; i++){
-    var delay = i*2;
-    var my_bar_div = document.getElementById("myBarDiv");
-    var div_temp = document.createElement("DIV");   // Create a <button> element
-    div_temp.classList.add("loading-bar");
-    div_temp.style.WebkitAnimationDelay = ""+delay+"s";
-    div_temp.style.animationDelay = ""+delay+"s";
+// var audio = new Audio('StarCloudBurst.mp3');
+// audio.play();
 
-    my_bar_div.appendChild(div_temp);               // Append <button> to <body>
-
-  }
-
-})();
+var x = document.getElementById("song1").autoplay;
